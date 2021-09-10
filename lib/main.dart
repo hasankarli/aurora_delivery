c841e9efd1ed22f54b133e4934f782579ee5ef22
hasankarli <karlihasann@gmail.com>
2021-09-10 14:48:33 +0300

    bug fixed progress download

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,14 @@ class SecondTab extends StatefulWidget {
 
 class _SecondTabState extends State<SecondTab> implements ViewPlayAsset {
   bool showLoading = false;
-  String statusText = "Mohon tunggu";
+  String progressText = "";
+  String totalSizeText = "";
+  String errorMessageText = "";
+  String checkAssetFolderPath = "";
+  String assetFail = "";
+  String assetNull = "";
+  String assetCompleted = "";
+  String assetStart = "";
   FlutterPlayAsset playAssetHelper = FlutterPlayAsset();
 
   @override
@@ -64,48 +71,63 @@ class _SecondTabState extends State<SecondTab> implements ViewPlayAsset {
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 48.0),
-                      child: Text(statusText),
-                    )
-                  ],
-                ),
-              ),
-            )
-          : Container(
-              child: InkWell(
-                onTap: (() async {
-                  setState(() {
-                    showLoading = true;
-                  });
-                  await playAssetHelper.init(this);
+                child: CircularProgressIndicator(),
+              ))
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: IconButton(
+                    icon: Icon(Icons.edit),
+                    iconSize: 32,
+                    onPressed: () async {
+                      setState(() {
+                        showLoading = true;
+                      });
+                      await playAssetHelper.init(this);
 
-                  print('process to loading page');
-                  await checkIfImageEditorAssetPackExist();
-                }),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.edit),
-                      iconSize: 32,
-                      onPressed: () {},
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Click to try module image editor',
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
+                      print('process to loading page');
+                      await checkIfImageEditorAssetPackExist();
+
+                      setState(() {
+                        showLoading = false;
+                      });
+                    },
+                  ),
                 ),
-              ),
+                SizedBox(
+                  height: 20,
+                ),
+                progressText.isNotEmpty
+                    ? _StatusText(value: progressText, title: 'progressText')
+                    : SizedBox(),
+                totalSizeText.isNotEmpty
+                    ? _StatusText(value: totalSizeText, title: 'totalSizeText')
+                    : SizedBox(),
+                errorMessageText.isNotEmpty
+                    ? _StatusText(
+                        value: errorMessageText, title: 'errorMessageText')
+                    : SizedBox(),
+                checkAssetFolderPath.isNotEmpty
+                    ? _StatusText(
+                        value: checkAssetFolderPath,
+                        title: 'checkAssetFolderPath')
+                    : SizedBox(),
+                assetFail.isNotEmpty
+                    ? _StatusText(value: assetFail, title: 'assetFail')
+                    : SizedBox(),
+                assetNull.isNotEmpty
+                    ? _StatusText(value: assetNull, title: 'assetNull')
+                    : SizedBox(),
+                assetCompleted.isNotEmpty
+                    ? _StatusText(
+                        value: assetCompleted, title: 'assetCompleted')
+                    : SizedBox(),
+                assetStart.isNotEmpty
+                    ? _StatusText(value: assetStart, title: 'assetStart')
+                    : SizedBox(),
+              ],
             ),
     );
   }
@@ -113,35 +135,89 @@ class _SecondTabState extends State<SecondTab> implements ViewPlayAsset {
   Future<void> checkIfImageEditorAssetPackExist() async {
     try {
       setState(() {
-        statusText = "Get asset directory";
+        progressText = "Get asset directory";
       });
       playAssetHelper.getAssetPath("editorassetpack");
     } catch (_) {
       print('checkIfImageEditorAssetPackExistError $_');
       setState(() {
-        statusText = "Error $_";
+        progressText = "Error $_";
       });
     }
   }
 
   @override
-  void onAssetPathFound(String path) {
-    setState(() {
-      statusText = "downloaded in $path";
-    });
-  }
-
-  @override
-  void onProcessLoadingAssetPath(String message) {
-    setState(() {
-      statusText = message;
-    });
-  }
-
-  @override
   void onProgressDownload(int percentage) {
     setState(() {
-      statusText = "Download asset $percentage%";
+      progressText = "Download asset $percentage";
     });
+  }
+
+  @override
+  void totalSize(int percentage) {
+    setState(() {
+      totalSizeText = "$percentage";
+    });
+  }
+
+  @override
+  void errorMessage(String error) {
+    setState(() {
+      errorMessageText = "$error";
+    });
+  }
+
+  @override
+  void onCheckAssetFolderPath(String path) {
+    setState(() {
+      checkAssetFolderPath = "$path";
+    });
+  }
+
+  @override
+  void onAssetFail(String message) {
+    setState(() {
+      assetFail = "$message";
+    });
+  }
+
+  @override
+  void onAssetStart(String message) {
+    setState(() {
+      assetStart = "$message";
+    });
+  }
+
+  @override
+  void onAssetCompleted(String message) {
+    setState(() {
+      assetCompleted = "$message";
+    });
+  }
+
+  @override
+  void onAssetNull(String message) {
+    setState(() {
+      assetNull = "$message";
+    });
+  }
+}
+
+class _StatusText extends StatelessWidget {
+  const _StatusText({
+    Key? key,
+    required this.value,
+    required this.title,
+  }) : super(key: key);
+
+  final String value;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Text("$title: $value"),
+    );
   }
 }
